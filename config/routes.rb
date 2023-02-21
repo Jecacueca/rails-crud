db62227683bf9a2d7218a3bd13623e0c20e9f311
Jess Silva Carvalho <jc.carvalho@outlook.com>
2023-02-21 10:55:12 +0000

    lecture demo

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # READ ALL
+  # get "restaurants", to: "restaurants#index"
+
+  # # CREATE
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # # READ ONE
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  # # UPDATE
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # DELETE
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+puts "Destroyed all restaurants"
+
+10.times do
+  Restaurant.create!(
+    address: Faker::Address.street_name,
+    name: Faker::FunnyName.name,
+    rating: rand(0..5)
+  )
+  puts "Created restaurants"
+end
