e0aeb91f2f36204cdec9d17eac265b7e8551eca8
Jess Silva Carvalho <jc.carvalho@outlook.com>
2023-05-30 15:58:09 +0100

    set re mote

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/restaurants', to: 'restaurants#index'
-  get '/restaurants/:id', to: 'restaurants#show'
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new' # to render new form
+  # post '/restaurants', to: 'restaurants#create'
+
+
+
+  # get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  resources :restaurants
 end
